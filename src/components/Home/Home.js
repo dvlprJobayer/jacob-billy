2fbbebb7b98b59f8826bbbfdbe2d23b254d1af1f
dvlprJobayer <nesarahammed019@gmail.com>
2022-04-17 18:52:43 +0600

    sign up page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner';
+import Service from '../Service/Service';
+import './Home.css';
 
 const Home = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('service.json').then(res => res.json()).then(data => setServices(data))
+    }, []);
+
     return (
-        <div>
+        <>
             <Banner />
-        </div>
+            <div className="container my-4 services-container">
+                <h2 className='text-center h1'>Want me to shoot you?</h2>
+                <div className="row mt-4">
+                    {
+                        services.map(service => <Service key={service.id} service={service} />)
+                    }
+                </div>
+            </div>
+        </>
     );
 };
 
---
+++ src/components/Service/Service.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({ service }) => {
+
+    const { name, picture, balance, desc1, desc2, desc3, desc4, desc5 } = service;
+
+    return (
+        <div className='col-lg-4 service'>
+            <img src={picture} alt="" />
+            <h3>{name}</h3>
+            <div className='text-center service-des'>
+                <p>{desc1}</p>
+                <p>{desc2}</p>
+                <p>{desc3}</p>
+                <p>{desc4}</p>
+                <p>{desc5}</p>
+            </div>
+            <h4 className='text-center my-3'>Price: <span>${balance}</span></h4>
+            <div className='text-center'>
+                <button className='btn btn-lg'>Book Now</button>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
